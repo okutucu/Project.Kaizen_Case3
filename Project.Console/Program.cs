78c19bdf218fc40030ba43df344eba054cfdcbcb
Oğuzhan <oktc95@gmail.com>
2023-01-06 03:03:43 +0300

    Add project files.

---
+++ Project.Console/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using Project.Console;
+
+
+// Google Cloud Vision API --> OCR (Optical Character Recognition) 
+// Masaüstünde bulunan json dosyasının  pathini verdik.
+string filePath = @"C:\Users\kaank\OneDrive\Desktop\json\json.txt";
+
+
+// Okuma işlemi için Streamreadr açtık. Using garbage collectorı tetikler kodla iş bittiksen bir süre sonra ram'den temizlenir.
+using (var sr = new StreamReader(filePath))
+{
+
+    // Jsonu okuduk ve ReadToEnd methodundan çıktısını string olarak aldık.
+    string json = sr.ReadToEnd();
+    sr.Close();
+
+    // Gelen json verisine uygun yapıyı karşılayacak sınıflarımızı oluşturduk ve DeserializeObject işlemini yaptık.
+    List<Receipt> receipts = JsonConvert.DeserializeObject<List<Receipt>>(json);
+
+
+
+    foreach (var item in receipts)
+    {
+        // descriptiondaki verilen karaktere göre parçalara ayırdık ve dizeye attık.
+        string[] lines = item.Description.Split('\n');
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+
+            Console.WriteLine($"{i + 1} {lines[i]}");
+        }
+
+    }   
+
+}
+
+
+Console.ReadLine();
---
+++ Project.Console/Vertice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Console
+{
+    public class Vertice
+    {
+         public int X { get; set; }
+         public int Y { get; set; }  
+    }
+}
